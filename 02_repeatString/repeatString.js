b87d34ff7e8bb4c45790c344a26f43ad6bb711bc
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-11 16:26:49 -0600

    Pass first test to repeat string 3 times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function() {
-
+   let text = 'hey' 
+   for (i = 0; i < 100; i++) {
+    text += text.concat(text);
+    return text;
+   }
 };
 
 // Do not edit below this line
