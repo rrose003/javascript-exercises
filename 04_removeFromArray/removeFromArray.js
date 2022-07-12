84ad6a7fd8bc53b57ffbf060b77bad3f8f9c4fc9
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-12 11:17:04 -0600

    Create while loop and test multiple values against array to remove unwanted elements, pass test 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,11 @@
-const removeFromArray = function(arr, value) {
-    var index = arr.indexOf(value);
-    if (index > -1){
-        arr.splice(index, 1);
+const removeFromArray = function(arr, value, value2, value3, value4) {
+    var i = 0;
+    while (i < arr.length) {
+        if(arr[i] === value || arr[i] === value2 || arr[i] === value3 || arr[i] === value4) {
+            arr.splice(i, 1)
+        } else {
+            ++i;
+        }
     }
     return arr;
 };
