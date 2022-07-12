dbe036a9078ecc906b2ed0ce6378f07ee718248d
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-12 16:03:51 -0600

    Pass test 2, works w/ punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,8 @@
 const palindromes = function (str) {
-    if (str === str.split('').reverse().join('')) {
-        return true;
-    } else {
-        return false;
-    }
+    let re = /[\W_]/g;
+    let lowRegStr = str.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
