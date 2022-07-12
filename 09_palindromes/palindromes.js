1e1270ef299d76c5a1ec4f72b23c4db5750c04db
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-12 15:50:22 -0600

    Pass test 1, works w/ single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    if (str === str.split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
