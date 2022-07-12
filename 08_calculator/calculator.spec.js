fc335f3a6b9c4f07efb3c2f62bd3839b7b1a576c
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-12 15:32:02 -0600

    Pass test 13, computes factorial of 1

--- 08_calculator/calculator.spec.js
+++ 08_calculator/calculator.spec.js
@@ -59,7 +59,7 @@ describe('factorial', () => {
 		expect(calculator.factorial(0)).toBe(1); // 0! = 1
 	});
 
-	test.skip('computes the factorial of 1', () => {
+	test('computes the factorial of 1', () => {
 		expect(calculator.factorial(1)).toBe(1);
 	});
 
