3cac70897a5b5b19a64c918561ab704dc2f46909
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-12 12:42:14 -0600

    Pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let lower;
+    let upper;
+    if (min > 0 && max > 0) {
+        if (min < max) {
+            lower = min;
+            upper = max;
+        } else {
+            lower = max;
+            upper = min;
+        }
+    } else {
+        return 'ERROR';
+    }
+    return ((upper*(upper+1) - ((lower-1)*lower)) / 2);
 };
 
 // Do not edit below this line
