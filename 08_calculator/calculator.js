96fce9a6ea3e0e921a9c5bcd314f10a370dbfa96
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-12 14:47:07 -0600

    Pass test 1, adds 0 and 0

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,23 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum;
+	if (arr == []) {
+    return 0;
+  } else if (typeof(arr[0]) == 'number' && typeof(arr[1] == 'undefined')) {
+    return arr;
+  } else {
+    for (i = 0; i < arr.length; i++) {
+       sum += arr[i];
+    }
+    return sum;
+  }
 };
 
 const multiply = function() {
