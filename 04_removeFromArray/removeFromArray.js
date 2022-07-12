1f1e6e6374c545e550f41bcb96c6f7192ed5b56c
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-12 10:20:20 -0600

    Code fails test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, item, index) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr.includes(item)[index]) {
+            arr.splice(item, i);
+            i--;
+        } else {
+            return arr;
+        }
+    } 
 };
 
 // Do not edit below this line
